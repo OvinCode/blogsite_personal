066ad500552e07b44932c795a8d8615196ef7326
OvinCode <crafty.abhi897@gmail.com>
2023-11-18 03:38:22 +0530

    - Changes made in Urls - Added Django Admin functionality - Added Image functionality to Gallery Page

--- ovin_blog/blog_app1/models.py
+++ ovin_blog/blog_app1/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import os
+from uuid import uuid4
+import random
+
+def upload_to(instance, filename):
+    # Get the file's extension
+    ext = filename.split('.')[-1]
+    random_number = random.randint(1, 9)
+    # Generate a unique filename
+    new_filename = f'gallery-image{random_number}.{ext}'
+    # Return the path where the file will be uploaded
+    return os.path.join('blog_app1/static/images/', new_filename)
+
 
 # Create your models here.
+class ImageModel(models.Model):
+    picname = models.TextField(blank=False,null=False)
+    image = models.ImageField(upload_to=upload_to)
+    # Other fields if needed
+
+    def __str__(self):
+        return self.picname if self.picname else f"ImageModel {self.id}"
